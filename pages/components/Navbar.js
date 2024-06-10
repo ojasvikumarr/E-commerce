e2264702bbec7c9fad6ce33ec9e715a038da87ad
Ojasvi Kumar <ojasvikumar007@gmail.com>
2024-06-10 17:30:53 +0530

    second commit

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,24 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaCartShopping } from "react-icons/fa6";
+import { HiOutlineShoppingCart } from "react-icons/hi2";
 const Navbar = () => {
     return (
         <>
-            <header className="text-gray-600 body-font">
-                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <header className=" text-gray-600 body-font">
+                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center shadow-md">
                     <Link href={'/'}>
                     <Image src="/codeswear-high-resolution-logo-transparent.png" width={100} height={100}/>
                     </Link>
                     <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-                        <a className="mr-5 hover:text-gray-900">T-shirts</a>
-                        <a className="mr-5 hover:text-gray-900">Hoodies</a>
-                        <a className="mr-5 hover:text-gray-900">Sweatshirts</a>
-                        <a className="mr-5 hover:text-gray-900">Mugs</a>
-                        <a className="mr-5 hover:text-gray-900">Zipper</a>
+                        <Link href={"/tshirts"} className="mr-5 hover:text-gray-900">T-shirts</Link>
+                        <Link href={"/hoodies"} className="mr-5 hover:text-gray-900">Hoodies</Link>
+                        <Link href={"/sweatshirts"} className="mr-5 hover:text-gray-900">Sweatshirts</Link>
+                        <Link href={"/mugs"} className="mr-5 hover:text-gray-900">Mugs</Link>
+                        <Link href={"/zipper"} className="mr-5 hover:text-gray-900">Zipper</Link>
                     </nav>
-                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Cart
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                        </svg>
+                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 w-12 h-8 rounded text-base mt-4 md:mt-0">
+                        {/* <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24"> */}
+                            {/* <path d="M5 12h14M12 5l7 7-7 7"></path> */}
+
+                            {/* <FaCartShopping className="text-2xl"/> */}
+                            <HiOutlineShoppingCart className="text-2xl" />
+                        {/* </svg> */}
                     </button>
                 </div>
             </header>
---
+++ pages/tshirts.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const tshirts = () => {
+  const products = [
+    { id: 1, category: 'CATEGORY', src: '/photo-1618677603544-51162346e165.webp', title: 'The Catalyzer', price: 16.00 },
+    { id: 2, category: 'CATEGORY', src: '/photo-1618677603544-51162346e165.webp', title: 'Shooting Stars', price: 21.15 },
+    { id: 3, category: 'CATEGORY', src: '/photo-1618677603544-51162346e165.webp', title: 'Neptune', price: 12.00 },
+    { id: 4, category: 'CATEGORY', src: '/photo-1618677603544-51162346e165.webp', title: 'The 400 Blows', price: 18.40 },
+    { id: 5, category: 'CATEGORY', src: '/photo-1618677603544-51162346e165.webp', title: 'The Catalyzer', price: 16.00 },
+    { id: 6, category: 'CATEGORY', src: '/photo-1618677603544-51162346e165.webp', title: 'Shooting Stars', price: 21.15 },
+    { id: 7, category: 'CATEGORY', src: '/photo-1618677603544-51162346e165.webp', title: 'Neptune', price: 12.00 },
+    { id: 8, category: 'CATEGORY', src: '/photo-1618677603544-51162346e165.webp', title: 'The 400 Blows', price: 18.40 },
+  ];
+
+  return (
+    <>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            {products.map((product) => (
+              <div key={product.id} className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-md ">
+                <Link href={`/product/tshirt`} className='shadow-lg' passHref>
+                  {/* <a className="block relative h-48 rounded overflow-hidden"> */}
+                    <Image
+                      alt="ecommerce"
+                      className="m-auto block h-[20vh] border-opacity-45"
+                      src={product.src}
+                      width={150}
+                      height={500}
+                    />
+                  {/* </a> */}
+                </Link>
+                <div className="mt-4 text-center">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{product.category}</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">{product.title}</h2>
+                  <p className="mt-1 text-center">${product.price.toFixed(2)}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default tshirts;
